4137b855d5e299dd5b25cb5b6b9f41302e0a000e
jscodeguy <im.still.at.school99@gmail.com>
2022-02-14 01:20:14 -0500

    wasnt able to get the game functioning but tried my best to complete as many objectives as i could

--- js/app.js
+++ js/app.js
@@ -13,90 +13,258 @@ const tile6 = document.querySelector('#tile6')
 const tile7 = document.querySelector('#tile7')
 const tile8 = document.querySelector('#tile8')
 const tile9 = document.querySelector('#tile9')
-
+//this array will hold the values of my game board tiles
+const tileArray =[tile1,tile2,tile3,tile4,tile5,tile6,tile7,tile8,tile9]
+let square = 0
+let playerTurn ='X'
 
 
 // A user should be able to click on different squares to make a move.
-    //this function will change the tile to an x or an o
+    //this loop will change the tile to an x or an o
+    
+// Every click will alternate between marking an X and O
+//we need to have a set of conditions that determine whos turn it is
+//we need to have a function that will mark the tile with an x and an o
+
+
+// for(i=0; i<tileArray.length;i++) { 
+//     tileArray[i].onclick = function () {
+//     const markTile = document.createElement('p')
+//     markTile.innerText = 'X'
+//     tileArray[i].appendChild(markTile)
+
+//     if (tileArray[i].firstChild){
+//         tileArray[i].removeChild(tileArray[i].firstChild)
+//     }
+// }}
     const changeTile1 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile1.appendChild(markTile)
-        let choice = 0
-        // if (choice === 0) {
-
-        // }
-    }
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile1.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile1.appendChild(markO)
+            playerTurn = 'X'
     
-    
+            }
+}
     const changeTile2 = () => {
     const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile2.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile2.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile2.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
+    // for(i=0; i<tileArray.length;i++) { 
+    //     tileArray[i].onclick = function () {
+    //     const markTile = document.createElement('p')
+    //     markTile.innerText = 'X'
+    //     tileArray[i].appendChild(markTile)
+
+    //     if (tileArray[i].firstChild){
+    //         tileArray[i].removeChild(tileArray[i].firstChild)
+    //     }
+        
+        
+    //     if (playerTurn ==='X' && playerTurn !== 'O') {
+    //         const markTile = document.createElement('p')
+    //             markTile.innerText = 'X'
+    //             tile2.appendChild(markTile)
+    //             playerTurn = 'O'
+    //             }
+    //             else {
+    //             markTile.innerText = 'O'
+    //             tile2.appendChild(markTile)
+    //             playerTurn = 'X'
+        
+    //             }
+    // }
+    // }
     }
     
     
     const changeTile3 = () => {
+    // const markTile = document.createElement('p')
+    //     markTile.innerText = 'X'
+    //     tile3.appendChild(markTile)
+    // for(i=0; i<tileArray.length;i++) { 
+    //     tileArray[i].onclick = function () {
+    //     const markTile = document.createElement('p')
+    //     markTile.innerText = 'X'
+    //     tileArray[i].appendChild(markTile)
+        
+        
     const markTile = document.createElement('p')
-        markTile.innerText = 'X'
-        tile3.appendChild(markTile)
-    }
+    markTile.innerText = 'X'
+    tile3.appendChild(markTile)
+if (playerTurn ==='X' && playerTurn !== 'O') {
+    //const markTile = document.createElement('p')
+       // markTile.innerText = 'X'
+        //tile3.appendChild(markTile)
+        playerTurn = 'O'
+        }
+        else {
+        const markO = document.createElement('p')
+        markO.innerText = 'O'
+        tile3.appendChild(markO)
+        playerTurn = 'X'
+
+        }
+}
+   // }
+   // }
+//}
     
     
     const changeTile4 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile4.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile4.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile4.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     const changeTile5 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile5.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile5.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile5.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     const changeTile6 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile6.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile6.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile6.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     const changeTile7 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile7.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile7.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile7.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     const changeTile8 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile8.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile8.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile8.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     const changeTile9 = () => {
-    const markTile = document.createElement('p')
+        const markTile = document.createElement('p')
         markTile.innerText = 'X'
         tile9.appendChild(markTile)
+    if (playerTurn ==='X' && playerTurn !== 'O') {
+        //const markTile = document.createElement('p')
+           // markTile.innerText = 'X'
+            //tile9.appendChild(markTile)
+            playerTurn = 'O'
+            }
+            else {
+            const markO = document.createElement('p')
+            markO.innerText = 'O'
+            tile9.appendChild(markO)
+            playerTurn = 'X'
+    
+            }
     }
     
     
     
     
-// Every click will alternate between marking an X and O
+
 // Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
 // A cell should not be able to be replayed once marked.
 // You should not be able to click remaining empty cells after the game is over.
 // Add a reset button that will clear the contents of the board.
     //this function will reset the board
 const reset = () => {
-
+    window.location.reload()
 }
-document.addEventListener('click', reset)
+resetButton.addEventListener('click', reset)
 // Display a message to indicate which turn is about to be played.
     //this function will declare the winner in an alert
 const winMessage = () => {
